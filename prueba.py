39a81bc33a1f853d1831c933a327a42889f71f6a
Jesus-Lievano <f.lievano@uniandes.edu.co>
2025-02-02 09:57:54 -0500

    Añadir Vraiable Aleatoria Exponencial

--- prueba.py
+++ prueba.py
@@ -1,3 +1,21 @@
 var = 12
 nuevo_cambio = 12
 aaa=1e12
+
+# Modificación del sagrado rostro
+
+import numpy as np
+
+lamb = 22
+
+n = 1000
+
+vals = np.random.exponential(scale = 1/lamb, size =n)
+
+import matplotlib.pyplot as plt
+
+count, bins, ignored = plt.hist(x=vals, bin = 30)
+plt.title("Histograma Distribución Exponencial de Martin")
+plt.xlabel("Tiempos Entre Arribos")
+plt.ylabel("Frecuencia")
+plt.show()
